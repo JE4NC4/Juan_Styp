b5070b6d09b7d81c2afc7737ea98512781dab989
Camilo Rojas <jcrojas596@misena.edu.co>
2022-02-17 21:59:35 -0500

    add clases 1-2-3

---
+++ Clase_16/JuanStyp/index.js
@@ -0,0 +1,1 @@
+document.write("<h1> Hola </h1>")
---
+++ Clase_16/JuanStyp_2/index1.js
@@ -0,0 +1,39 @@
+// var carName = "Volvo"
+
+// document.write(carName);
+
+// document.getElementById("demo").innerHTML = "hello world"
+
+
+// let x = 10;
+// {
+//     let x = 20;
+//     document.write("Dentro del bloque x: ", x, " ");
+// }
+
+// document.write("Fuera del bloque x: ", x, " ");
+
+// const pi = 3.1416;
+// document.write("El numero pi fuera del bloque es: ", pi, "<br>");
+
+// {
+//     const pi = 3.141519;
+//     document.write("El numero pi en el bloque es: ", pi, "<br>");
+// }
+
+// const año = 2022;
+// document.write("El año actual es ", año, "<br>");
+// let año_nac = 1980;
+// document.write("El año de nacido es ", año_nac, "<br>");
+// año_nac = 2003;
+// document.write("El año de nacido es ", año_nac, "<br>");
+// año_nac = 2004;
+// document.write("El año de nacido es ", año_nac, "<br>");
+
+let x = 2;
+let total = (((x+1+2+3-1)*3)/2)*(x**x);
+document.write("El total es: ", total, "<br>")
+
+const nombre = "Jairo Camilo";
+let conc = total+total+nombre+total;
+document.write(conc)
